d86ca29a58c3af9add9ae5286d2feb6d860b26c7
Gopal <2024d2r034@mietjammu.in>
2026-08-19 00:01:53 +0530

    POSt method to create items

---
+++ sample3.py
@@ -0,0 +1,24 @@
+#HTTPS methods :- GET,POST,PUT,DELETE,PATCH
+
+#GET :- read the resource  (READ)
+#POST :- create the resource and send something to server  (CREATE)
+#PUT :- update the resource completely (UPDATE)
+#DELETE :- delete the resource (DELETE)
+#PATCH :- partially update the resource (PARIIALLY UPDATE)
+
+# for POST method  -> client send data alongwith the path parameter {No need of data_ID}
+# for PUT method  -> client send data and data_ID alongwith the path parameter {need of data_ID}
+# for DELETE method -> client send  only data_ID alongwith the path parameter {need of data_ID}
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from data import products
+
+app=FastAPI()
+@app.post("/create items ")
+def create_item(id:int=None,name:str=None,price:float=None):
+    if id==None or name==None or price==None:
+        return "Please enter all the fields"
+    
+    products.append({"id":id,"name":name,"price":price})
+    return "Item created successfully"
