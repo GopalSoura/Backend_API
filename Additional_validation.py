9a56308e366ffe1b1f442fae3b7b28eae13d2a2e
Gopal <2024d2r034@mietjammu.in>
2026-08-21 11:17:21 +0530

    default parameter for query

--- Additional_validation.py
+++ Additional_validation.py
@@ -18,20 +18,30 @@ async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
 
 '''
 
-# Additional string validator to validate the length and pattern of the Query in string using annotated lib and query
+
 from typing import Annotated
 
 from fastapi import FastAPI, Query
 
 app = FastAPI()
 
-
+# Additional string validator to validate the length and pattern of the Query in string using annotated lib and quer
+'''
 @app.get("/items/")
 async def read_items(
     q: Annotated[
         str | None, Query(min_length=3, max_length=50, pattern="\\b[A-Za-z]*\\b")
     ] = None,
 ):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+        print(results)
+    return results
+'''
+
+@app.get("/items/")
+async def read_items(q: Annotated[str, Query(min_length=3)] = "fixedquery"):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
