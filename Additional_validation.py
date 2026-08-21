80e8fe52bb6500732085683eb72e07a5440c3240
Gopal <2024d2r034@mietjammu.in>
2026-08-21 11:11:52 +0530

    added length and pattern validation

--- Additional_validation.py
+++ Additional_validation.py
@@ -1,3 +1,4 @@
+'''
 from typing import Annotated
 
 from fastapi import FastAPI, Query
@@ -13,4 +14,26 @@ async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
     for i, j in results.items():
         print(i)
         print(j)
+    return results
+
+'''
+
+# Additional string validator to validate the length and pattern of the Query in string using annotated lib and query
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None, Query(min_length=3, max_length=50, pattern="\\b[A-Za-z]*\\b")
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+        print(results)
     return results
