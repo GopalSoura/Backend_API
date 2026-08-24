e3c407b59fe565d4caf2a11e6765a255a80f262c
Gopal <2024d2r034@mietjammu.in>
2026-08-25 00:00:08 +0530

    custom validation

--- Query_parameter_and_string_validation.py
+++ Query_parameter_and_string_validation.py
@@ -74,7 +74,7 @@ async def read_items(
         return {"hidden_query": hidden_query}
     else:
         return {"hidden_query": "Not found"}
-        
+
 @app.get("/items_hidden")
 def get_items(
     name: str,
@@ -84,3 +84,31 @@ def get_items(
         "name": name,
         "old_name": old_name
     }
+# Custom validation 
+import random
+from pydantic import AfterValidator
+
+app = FastAPI()
+
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
+
+
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
+
+
+@app.get("/items/")
+async def read_items(
+    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
+):
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id": id, "name": item}
