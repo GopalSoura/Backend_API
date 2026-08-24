cbdfab63b0ca1b069f54d94354266185381976a0
Gopal <2024d2r034@mietjammu.in>
2026-08-24 23:45:02 +0530

    Hidden query

--- Query_parameter_and_string_validation.py
+++ Query_parameter_and_string_validation.py
@@ -61,3 +61,16 @@ async def read_items(
     if q:
         results.update({"q": q})
     return results
+
+#To exclude a query parameter from the generated OpenAPI schema
+# (and thus, from the automatic documentation systems),
+#  set the parameter include_in_schema of Query to False
+
+@app.get("/items/")
+async def read_items(
+    hidden_query: Annotated[str | None, Query(include_in_schema=False)] = None,
+):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    else:
+        return {"hidden_query": "Not found"}
