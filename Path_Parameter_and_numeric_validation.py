2014293f69502dfe3a738a4b66fba5a7aa3e40e6
Gopal <2024d2r034@mietjammu.in>
2026-08-27 23:06:47 +0530

    * as default parameter

--- Path_Parameter_and_numeric_validation.py
+++ Path_Parameter_and_numeric_validation.py
@@ -16,3 +16,11 @@ async def read_items(
         results.update({"q": q})
         print(results)
     return results
+
+
+@app.get("/item/{item_id}")
+async def read_items(*, item_id: int = Path(title="The ID of the item to get"), q: str):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
