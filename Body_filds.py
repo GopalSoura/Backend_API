91e38e3debf0627f6fe17c6ae8b1171f98eeb5f4
Gopal <2024d2r034@mietjammu.in>
2026-08-30 23:56:34 +0530

    path corrected

--- Body_filds.py
+++ Body_filds.py
@@ -23,15 +23,16 @@ async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
 
 # implementing path and query at the same time 
 @app.get("/items_search/{item_id}")
-async def get_items(item_id:Annotated[int,Path(
+async def get_items(item_id:Annotated[int|None,Path(
     title="The id of the item",
     description="Put the item id",
     gt=0,le=1000)],
 
     item_name:Annotated[str|None,Query(
     title="the name of the item",
-    description="Put the name of the iten",)
-    ]=None
+    description="Put the name of the iten")
+    ]
     ):
+    print(item_id,item_name)
     result={"item_id":item_id,"item_name":item_name}
     return result
