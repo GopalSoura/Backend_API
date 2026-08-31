00faac74b8f25fa4cc40ee831c3de1bd10b37b36
Gopal <2024d2r034@mietjammu.in>
2026-08-31 11:53:29 +0530

    Commented

--- Declare_Request.py
+++ Declare_Request.py
@@ -1,3 +1,4 @@
+# Additional information for Json Schema and OpenApi Schema with time,description,example,information
 from time import time
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -31,3 +32,16 @@ class Item(BaseModel):
 async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
+
+# Output
+'''
+"Item": {
+    "properties": {
+        "name": {"type": "string"},
+        "description": {"type": "string / null"},
+        "price": {"type": "number"},
+        "tax": {"type": "number / null"}
+    },
+    "required": ["name", "price"]
+}
+'''
